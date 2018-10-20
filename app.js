72de8ef6ab3afaa2264d1d154d835d124482464e
arjun <arjunagarwal1998@gmail.com>
2018-10-21 02:47:11 +0530

    json

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const bp = require("body-parser");
 
 app.use(require("cors")());
 
-app.engine('hbs',hbs());
-app.set("view engine",'hbs');
+// app.engine('hbs',hbs());
+app.set("view engine",'ejs');
 app.use(bp.json());
 app.use(bp.urlencoded({extended:false}));
 app.use(express.static("static"));
 
 
-app.get("/",(req,res)=>{
-    res.render("index")
-});
+app.post('/',(req,res,next)=>{
+    res.render('index',{data:req.body});
+}); 
 
 
 app.listen(process.env.PORT || 3000,()=>console.log("Listening...."))
